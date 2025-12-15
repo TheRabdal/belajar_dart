8c2358144e7a5687d6af8ba36a9ece81596cab2d
TheRabdal <kautsarrabdal@gmail.com>
2025-12-15 09:55:45 +0700

    belajar_dart

--- lib/variable/late.dart
+++ lib/variable/late.dart
@@ -1,13 +1,28 @@
+late String namaLengkap;
+
 void main() {
   var value = getValue();
   print('Variable sudah dibuat');
   print(value);
 }
 
-String getValue() {}
+String getValue() {
+  print('getValue() dipanggil');
+  return 'kautsar';
+}
 
 /*
 variable akan dideklarasikan valuenya ketika variable itu dibuat
 namun kadang ada kasus dimana kita ingin variable dideklarasikan nanti saja
 ketika variable itu diakses, jika tidak diakses maka tidak di deklarasikan 
+contonya : 
+ 
+late String namaLengkap; //deklarasi dengan late/nantisaja
+
+void main() {
+  
+  namaLengkap = 'Muhammad Kautsar Rizqi Abdal'; //inisialisasi setelah deklarasi
+  
+  print(namaLengkap); //Output: Muhammad Kautsar Rizqi Abdal
+}
 */
