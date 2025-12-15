c4f87df1383d45c9b5c9448a30d3a4167da9de3d
TheRabdal <kautsarrabdal@gmail.com>
2025-12-15 10:02:38 +0700

    belajar_dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-//dart main function
-void main() {
-  //function print dengan parameter string/text,
-  //dan data tersebut bisa menggunakan kutip satu atau kutip dua
+void main() {  //dart main function
+  /*
+  function print dengan parameter string/text,
+  dan data tersebut bisa menggunakan kutip satu atau kutip dua
+  */
   print('Hello Word!');
 }
